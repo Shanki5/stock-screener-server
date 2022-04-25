6378b6320a6184bc87575f099abc7d730147e408
Shankrith Chokkalingam <shankrith1618@gmail.com>
2022-04-25 22:32:31 +0530

    Added get all stocks api route

--- routes/api/stocks.js
+++ routes/api/stocks.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const getCompanyOverview = require('../../utils/stock');
 const Stock = require('../../models/Stocks');
 
-// @route POST api/stocks
+// @route GET api/stocks
+// @description get all stocks in db
+// @access Public
+router.get('/', (req, res) => {
+	Stock.find()
+		.then(stocks => res.json(stocks))
+		.catch(err => res.status(404).json({ nostocksfound: 'No Stocks found' }));
+});
+
+
+
+// @route POST api/stocks/:symbol
 // @description add new stock to DB
 // @access Public
 router.post('/:id', (req, res) => {
